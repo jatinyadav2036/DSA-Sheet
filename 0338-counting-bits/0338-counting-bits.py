553dde736ba3a1e9bc5e064a85ef42843ca871e1
Jatin Yadav <jatinya21@gmail.com>
2026-09-16 16:59:56 +0530

    Time: 6 ms (77.06%), Space: 17.7 MB (55.39%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        ans = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            ans[i] = ans[i & (i - 1)] + 1
+
+        return ans
